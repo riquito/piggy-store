6b323c427174352ee68580554a5a6dcd5be6a970
Riccardo Attilio Galli <riccardo@sideralis.org>
2017-02-04 17:07:22 -0800

    Add empty route to list user files

--- piggy_store/app.py
+++ piggy_store/app.py
@@ -1,9 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, abort, make_response
+from flask_json import FlaskJSON, JsonError, json_response, as_json
 
-@app.route("/")
-def index():
-    return "<h1>Hello World</h1>"
+app = Flask(__name__)
+FlaskJSON(app)
+
+@app.route('/user/<username>', methods=['GET'])
+def list_user_files(username):
+    abort(401)
+
+@app.errorhandler(401)
+@as_json
+def user_unauthorized(e):
+    return {
+        'error': {
+            'code': e.code,
+            'message': e.name
+        }
+    }, e.code
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
