1699a84114dcbd06ea1406cd08b55346cccb7a90
Riccardo Attilio Galli <riccardo@sideralis.org>
2017-02-09 21:30:08 -0800

    Return every error response as application/json

--- piggy_store/app.py
+++ piggy_store/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, abort, request
 from flask_json import FlaskJSON, as_json
+import werkzeug
 
 from piggy_store.storage.users import user_storage, User
 from piggy_store.validators import new_user_validator, auth_user_validator
@@ -7,6 +8,7 @@ from piggy_store.exceptions import PiggyStoreError, UserDoesNotExistError, Chall
 from piggy_store.authentication import generate_auth_token, assert_user_challenge_match
 
 app = Flask(__name__)
+app.config['TRAP_HTTP_EXCEPTIONS']=True
 FlaskJSON(app)
 
 @app.route('/user/<username>', methods=['GET'])
@@ -38,35 +40,38 @@ def auth_user(username):
     }
 
 
-@app.errorhandler(401)
-@as_json
-def user_unauthorized(e):
-    return {
-        'error': {
-            'code': e.code,
-            'message': e.name
-        }
-    }, e.code
-
 @app.errorhandler(UserDoesNotExistError)
-@as_json
 def handle_user_does_not_exist_error(e):
-    return {
-        'error': {
-            'code': e.code,
-            'message': e.message
-        }
-    }, 404
+    return make_error_response(404, e.code, e.message)
 
 @app.errorhandler(PiggyStoreError)
-@as_json
 def handle_piggy_store_errors(e):
+    return make_error_response(409, e.code, e.message)
+
+def on_flask_http_exception(e):
+    if isinstance(e, werkzeug.exceptions.HTTPException):
+        return make_error_response(e.code, e.code, e.name)
+    else:
+        # happens when there was an error handling the exception
+        return make_error_response(500, 500, 'Internal Server Error')
+
+@app.errorhandler(Exception)
+def on_error(e):
+    return make_error_response(500, 500, 'Internal Server Error')
+
+@as_json
+def make_error_response(status, subcode, message):
     return {
         'error': {
-            'code': e.code,
-            'message': e.message
-        }
-    }, 409
+            'code': subcode,
+            'message': message
+        },
+        'status': status
+    }, status
+
+
+for werkzeugException in werkzeug.exceptions.default_exceptions:
+    app.register_error_handler(werkzeugException, on_flask_http_exception)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
