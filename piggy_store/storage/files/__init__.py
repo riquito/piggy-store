b193fc3a22eaf11f99510592eaaff8ec3db96e01
Riccardo Attilio Galli <riccardo@sideralis.org>
2017-03-26 02:57:52 -0700

    By default put prefix users files with 'users/'

--- piggy_store/storage/files/__init__.py
+++ piggy_store/storage/files/__init__.py
@@ -4,6 +4,8 @@ from piggy_store.config import config
 if config['storage']['files'] == 's3':
     from piggy_store.storage.files.s3_storage import S3Storage
     def access_file_storage(options):
+        if options['user_dir'] != 'admin$':
+            options['user_dir'] = 'users/' + options['user_dir']
         storage = S3Storage(dict(
             url_expire_after = config['files']['download_url_expire_after'],
             **config['s3'],
