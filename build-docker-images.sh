f9ec699505611e3f4267181224a7d81429901021
Riccardo Attilio Galli <riccardo@sideralis.org>
2017-05-08 02:21:44 -0700

    Build the docker base image only if it's missing

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 set -e
-packer.io build -only piggy-store-base-image build/packer-templates/docker-alpine-piggy-store.json
+
+# build a base image only if it's missing
+if [ $(docker images --format '{{.Repository}}:{{.Tag}}' | grep 'riquito/piggy-store-base-image-alpine:latest' | wc -l) -eq 0 ]; then
+    packer.io build -only piggy-store-base-image build/packer-templates/docker-alpine-piggy-store.json
+fi
+
 packer.io build -only docker-piggy-store build/packer-templates/docker-alpine-piggy-store.json
