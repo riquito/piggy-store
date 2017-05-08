22471a5f4689cfe2a78ecc024b961012d83e57be
Riccardo Attilio Galli <riccardo@sideralis.org>
2017-05-08 01:43:19 -0700

    Remove bash-only features from the image builder

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-set -eo pipefail
+set -e
 packer.io build -only piggy-store-base-image build/packer-templates/docker-alpine-piggy-store.json
 packer.io build -only docker-piggy-store build/packer-templates/docker-alpine-piggy-store.json
